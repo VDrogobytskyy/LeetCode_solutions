cfac54252d3db61153e3a04efbb346c3b61cce1b
Drogobytskyy <drogobytskyy.volodymyr@lll.kpi.ua>
2025-09-01 19:15:45 +0300

    Remove_element

---
+++ Easy_tasks/Remove_element.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+
+class Solution {
+    public:
+        int removeElement(std::vector<int> &nums, int val) {
+            std::vector<int> temp;
+            for(int i = 0; i < nums.size(); i++){
+                if (nums[i] != val){
+                    temp.push_back(nums[i]);
+                }
+            }   
+            nums = temp;
+            return temp.size();
+        }
+};
+
+
+
+int main(void){
+    std::cout << std::endl;
+    Solution s1;
+    std::vector<int> check = {1, 2, 3, 3, 3, 3, 5};
+    int result = s1.removeElement(check, 3);
+
+    std::cout << "len is: " << result << std::endl;
+
+
+    std::cout << std::endl;
+    return 0;
+}
